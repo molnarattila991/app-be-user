fe4d15ffa7eb2b50b922a0694160cfbb8c448fb4
molnattila991 <molnarattila991@gmail.com>
2022-07-12 11:10:15 +0200

    Updated mongoose options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServicesModule } from './services/services.module';
@@ -7,7 +7,11 @@ import { ServicesModule } from './services/services.module';
 @Module({
   imports: [
     ServicesModule,
-    MongooseModule.forRoot(process.env["NEST_MONGODB_CONNECTION_STRING"], <any>{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: "1" })
+    MongooseModule.forRoot(process.env["NEST_MONGODB_CONNECTION_STRING"],
+      <MongooseModuleOptions>{
+        useNewUrlParser: true, useUnifiedTopology: true, serverApi: "1",
+        dbName: process.env["NEST_MONGODB_DB_NAME"]
+      })
   ],
   controllers: [AppController],
   providers: [AppService],
